3810be6be1e972d04904494e33ad23fd9f8f8302
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-12-05 18:30:32 +0530

    updates

---
+++ app/Contact/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="w-full h-[70vh] bg-slate-800 flex items-center">
+        <h3 className="text-6xl font-bold text-white ml-40">CONTACT US</h3>
+      </div>
+      <div className="flex ">
+        <div className="w-[20vw] h-[15vh] border">Map area</div>
+        <div>
+          <span>Contact us ------</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
