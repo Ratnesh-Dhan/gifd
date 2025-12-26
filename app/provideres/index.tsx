88a43a73c15db701500419b9556b671ac8474659
lucky dhan <adityadevdhan@adityas-MacBook-Air.local>
2025-12-26 21:09:05 +0530

    fixing prover issue

--- app/provideres/index.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { DeviceProvider } from "./Devicecontext";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <DeviceProvider>{children}</DeviceProvider>;
-}
